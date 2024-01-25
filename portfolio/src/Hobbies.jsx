13707d3cff279d79a14737779651d38044c3f802
Siddharth-afk <gupta.siddharth20@gmail.com>
2024-01-25 23:29:07 +0530

    added hobbies jsx file

---
+++ portfolio/src/Hobbies.jsx
@@ -0,0 +1,15 @@
+export const Hobbies = () => {
+  return (
+    <div className="flex">       
+        <div className="grid grid-cols-2 h-[100vh] w-[100vw]">
+            <div className="bg-gradient-to-b from-cyan-950 from-1% to-black h-full w-full">
+
+            </div>
+
+            <div className="bg-slate-100 h-full w-full"> 
+                bye
+            </div>
+        </div>
+    </div>
+  )
+}
