0db1d83394843130ffe2e32e960e807ca0c63958
Siddharth-afk <gupta.siddharth20@gmail.com>
2024-01-26 21:47:15 +0530

    updated the hobbies files with stuff.

--- portfolio/src/Hobbies.jsx
+++ portfolio/src/Hobbies.jsx
@@ -6,8 +6,17 @@ export const Hobbies = () => {
 
             </div>
 
-            <div className="bg-slate-100 h-full w-full"> 
-                bye
+            <div className="bg-slate-100 h-full w-full flex"> 
+                <div className="flex w-[70%] h-[85%] bg-slate-200 mx-auto my-auto rounded-md">
+                  <div className="grid grid-rows-2 mx-auto px-8 py-8">
+                    <div>
+                      <p className="font-semibold text-4xl">Hobbies and Personal Interests</p>
+                    </div>
+                    <div>
+                      <p>Other than the technical stuff, I like to dabble in photography as well as a bit of editing, writing and various different thing in the content creation space! I'm a big fan of fiction novels, manga and gaming. I'm actively engaged in the tech community and always up to date with the latest tech, software or product.</p>
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
